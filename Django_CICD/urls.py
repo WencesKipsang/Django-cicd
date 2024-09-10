b8d3782fa4f2526491e70410224e763db9c89956
Wences Kipsang <cheruiyotwences@gmail.com>
2024-09-10 04:56:33 +0300

    third commit

--- Django_CICD/urls.py
+++ Django_CICD/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from cicd import urls as you
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cicd/hey/', include(you)),
 ]
